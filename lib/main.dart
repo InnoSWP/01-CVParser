047a7aadb4cd572fe0c53caafbc0fc341aa83856
Senopiece <shichiromail@gmail.com>
2022-06-06 22:21:43 +0300

    rm comment

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class CVParserApp extends StatelessWidget {
   const CVParserApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
