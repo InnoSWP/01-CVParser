154683e8e4f08b4964639c3744ad18fc11aeb9d9
KonstFed <konstantin_fedorov_03@mail.ru>
2022-06-08 19:34:02 +0300

    cards architecture is done

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -3,6 +3,65 @@ import 'package:flutter/material.dart';
 import 'package:cvparser_b21_01/misc.dart';
 import 'package:flutter_svg/svg.dart';
 
+class CardWidget extends StatefulWidget {
+  // right now it only accepts title, but you can add more
+  // arguments to be accepted by this widget
+  CardWidget({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _CardWidgetState createState() => _CardWidgetState();
+}
+
+class _CardWidgetState extends State<CardWidget> {
+  // responsible for toggle
+  bool _showData = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 10.0),
+          // list card containing country name
+          GestureDetector(
+              onTap: (){
+                setState(() => _showData = !_showData);
+              },
+              child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [BoxShadow(color: Colors.grey, offset: Offset(0.0, 3.0))]
+                  ),
+                  child: Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            // add your other icon here
+                            Text(widget.title)
+                          ]
+                      )
+                  )
+              )
+          ),
+
+          // this is the company card which is toggling based upon the bool
+          _showData ? Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: ['Kia','Samsung'].map((e){
+                // make changes in the UI here for your company card
+                return Card(child: Text(e));
+              }).toList()
+          ) : SizedBox() // else blank
+        ]
+    );
+  }
+}
+
 class MainPage extends StatelessWidget {
   static const _desiredPadding = 18.0;
   const MainPage({Key? key}) : super(key: key);
@@ -32,13 +91,15 @@ class MainPage extends StatelessWidget {
   Widget buildParseResult(BuildContext context) {
     return Expanded(
       child: Center(
-        child: ListView(
-          children: [Row(
-            children: [Text("label-first"),TextButton(onPressed: ()=>"aboba",child:Text("giga-button"))],
-          ),Row(
-            children: [Text("label-some-bullshit"),TextButton(onPressed: ()=>"aboba",child:Text("giga-button"))],
-          )],
-        )
+        child: Padding(
+          child:Column(
+              children: ['Korea', 'China', 'Japan', 'USA', 'India'].map((country){
+                // returning the CardWidget passing only title
+                return CardWidget(title: country);
+              }).toList()
+          ),
+          padding: EdgeInsets.all(12)
+        ),
       ),
     );
   }
