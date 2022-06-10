b39db973edc173fe29953fc8ea5ae40f7713c202
Nikita Grigorenko <57573137+NikitaGrigorenko@users.noreply.github.com>
2022-06-10 15:24:39 +0300

    Merge Errors Fixed

--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -6,13 +6,82 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
+import '../colors.dart';
+
+class CardWidget extends StatefulWidget {
+  // right now it only accepts title, but you can add more
+  // arguments to be accepted by this widget
+  const CardWidget({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _CardWidgetState createState() => _CardWidgetState();
+}
+
+class _CardWidgetState extends State<CardWidget> {
+  // responsible for toggle
+  bool _showData = false;
+  bool aboba = true;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 10.0),
+          // list card containing country name
+          GestureDetector(
+              onTap: (){
+                setState(() => _showData = !_showData);
+                setState(() => aboba = !aboba);
+              },
+              child: Container(
+                  width: 900,
+                  height: 100,
+                  margin: const EdgeInsets.symmetric(vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                  child: Padding(
+                      padding: EdgeInsets.fromLTRB(15, 2, 5, 5),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(widget.title, style: TextStyle(fontSize: 60, fontFamily: "Eczar", fontWeight: FontWeight.w400, color:  colorTextSmoothBlack)),
+                            if (aboba)
+                              Icon(Icons.keyboard_arrow_down_rounded, size: 55, color: colorSecondaryGreenPlant)
+                            else
+                              Icon(Icons.keyboard_arrow_up_rounded, size: 55, color: colorSecondaryGreenPlant)
+                          ]
+                      )
+                  )
+              )
+          ),
+
+          // this is the company card which is toggling based upon the bool
+          _showData ? Container(
+              padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: ['Java','Python', 'C++', 'Web Development', 'AWS', 'FPGA'].map((e){
+                    // make changes in the UI here for your company card
+                    return Card(child:Text(e, style: TextStyle(fontSize: 20, fontFamily: 'Merriweather', color: colorTextSmoothBlack)));
+                  }).toList()
+              )
+          ) : SizedBox() // else blank
+        ]
+    );
+  }
+}
+
 class MainPage extends StatelessWidget {
   static const _desiredPadding = 18.0;
-  const MainPage({Key? key}) : super(key: key);
+  MainPage({Key? key}) : super(key: key);
   final controller = Get.put(MainPageController());
   final keyLookup = Get.find<KeyListener>();
-
-  MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,31 +97,116 @@ class MainPage extends StatelessWidget {
     );
   }
 
-  Widget buildParseResult(BuildContext context) {
-    return const Expanded(
-      child: Center(
-        child: CircularProgressIndicator(),
+
+  Widget buildLeft(BuildContext context){
+    return Container(
+      child: Column(
+        children: [
+          Row(
+              children: [logo(context), buildContact(context)]
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(50, 0, 50, 50),
+              child: buildParseResult(context),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+            child: buildBottomLeft(context),
+          )
+        ],
       ),
     );
   }
 
-  Widget buildRightTab(BuildContext context) {
+  Widget logo(BuildContext context){
     return Container(
-      padding: const EdgeInsets.all(_desiredPadding),
-      width: 400,
-      color: Theme.of(context).colorScheme.secondary,
+      padding: EdgeInsets.fromLTRB(0, 30, 125, 0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          buildTopBar(context),
-          const SizedBox(height: _desiredPadding),
-          Expanded(
-            child: buildFileExplorer(context),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Text('CVParser \niExtract', style: TextStyle(
+            fontSize: 60,
+            fontFamily: 'Eczar',
+            fontWeight: FontWeight.w600,
+            color: colorPrimaryRedCaramelDark,
+            height: 0.9,
           ),
-          const SizedBox(height: _desiredPadding),
-          buildBottomBar(context),
+          ),
         ],
       ),
+    );
+  }
+
+
+  Widget buildContact(BuildContext context){
+    return Container(
+      width: 505,
+      height: 100,
+      padding: EdgeInsets.fromLTRB(10, 0, 0, 5),
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.onSecondary,
+          borderRadius: BorderRadius.circular(10)
+      ),
+      child: Container(
+          width: 400,
+          height: 80,
+          child: Row(
+            children: [Column(
+                children: const [
+                  Text('Konstantin Fedorov', textAlign: TextAlign.start, style: TextStyle(height: 1.5, fontSize: 50, fontFamily: "Eczar", fontWeight: FontWeight.w400, color:  colorTextSmoothBlack)),
+                  Text('k.fedorov@innopolis.university   +79221994815', textAlign: TextAlign.start, style: TextStyle(height: 0.2, fontSize: 20.2, fontFamily: "Eczar", fontWeight: FontWeight.w400, color:  colorTextSmoothBlack)),
+                ]
+            ), Icon(Icons.account_circle, size: 60, color: colorPrimaryRedCaramelDark)],
+          )
+      ),
+    );
+  }
+
+
+  Widget buildBottomLeft(BuildContext context){
+    return Container(
+        width: 900,
+        height: 50,
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(10)
+        ),
+
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(15, 2, 5, 0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: [
+                  Text('Pdf1', style: TextStyle(height: 1.3, fontSize: 45, fontFamily: "Eczar", fontWeight: FontWeight.w400, color: colorSurfaceSmoothGreenPlant)),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all<Size>(const Size(250, 35)),
+                    ),
+                    onPressed: () {},
+                    child: const Text("EXPORT AS JSON"),
+                  ),
+                ]
+            )
+        )
+    );
+  }
+
+
+
+  Widget buildParseResult(BuildContext context) {
+    return Container(
+      //padding: EdgeInsets.all(50),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child:Column(
+              children: ['Skills', 'Organization', 'Language', 'Countries', 'Publication', 'Links'].map((country){
+                return CardWidget(title: country);
+              }).toList()
+          ),
+        )
     );
   }
 
@@ -81,7 +235,7 @@ class MainPage extends StatelessWidget {
 
   // 3 TODO (uploading cv): merge ui
   // 4 TODO (uploading cv): bind merged ui with controller
-  Widget buildPdfIconButton(String name, bool isSelected) {
+  Widget buildPdfIconButton(int position) {
     // weak TODO: separate class and optimize rebuilds with no changes
     final index = controller.cvsS.keys.elementAt(position);
     final tile = controller.cvsS[index]!;
@@ -155,7 +309,7 @@ class MainPage extends StatelessWidget {
   Widget buildRightTab(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(_desiredPadding),
-      width: 400,
+      width: 520,
       color: Theme.of(context).colorScheme.secondary,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
