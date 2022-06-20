12782695d50c0fee262fba218f6b35d9e597c79d
Nikita Grigorenko <57573137+NikitaGrigorenko@users.noreply.github.com>
2022-06-20 14:34:16 +0300

    Update bottom_bar.dart

--- lib/views/main_page/right_panel/bottom_bar.dart
+++ lib/views/main_page/right_panel/bottom_bar.dart
@@ -56,31 +56,38 @@ class BottomBar extends GetView<MainPageController> {
           ],
         ),
         const SizedBox(height: 18.0),
-        // Row(
-        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //   children: [
-        //     ElevatedButton(
-        //       style: ButtonStyle(
-        //         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-        //             RoundedRectangleBorder(
-        //               borderRadius: BorderRadius.circular(10),
-        //             )),
-        //       ),
-        //       onPressed: controller.selectParsed,
-        //       child: const Text("SELECT PARSED"),
-        //     ),
-        //     ElevatedButton(
-        //       style: ButtonStyle(
-        //         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-        //             RoundedRectangleBorder(
-        //               borderRadius: BorderRadius.circular(10),
-        //             )),
-        //       ),
-        //       onPressed: controller.invertSelection,
-        //       child: const Text("INVERT SELECTION"),
-        //     ),
-        //   ],
-        // ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).colorScheme.primary),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+                fixedSize: MaterialStateProperty.all<Size>(const Size(250, 50)),
+              ),
+              onPressed: controller.invertSelection,
+              child: Text("INVERT SELECTION", style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontWeight: FontWeight.w600)),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).colorScheme.onSurface),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Theme.of(context).colorScheme.primary, width: 2),
+                    )),
+                fixedSize: MaterialStateProperty.all<Size>(const Size(175, 50)),
+              ),
+              onPressed: controller.selectParsed,
+              child: Text("SELECT PARSED", style: TextStyle(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.w600)),
+            ),
+          ],
+        ),
       ],
     );
   }
